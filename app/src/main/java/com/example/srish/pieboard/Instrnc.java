b80558ed049db9519e997c45cb5b369e09d091db
aks05 <ayushthakur05102000@gmail.com>
2019-02-20 17:15:18 +0530

    5.0.0

--- app/src/main/java/com/example/srish/pieboard/Instrnc.java
+++ app/src/main/java/com/example/srish/pieboard/Instrnc.java
@@ -1,6 +1,5 @@
 package com.example.srish.pieboard;
 
-import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
